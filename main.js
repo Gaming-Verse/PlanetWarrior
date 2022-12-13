50183c21235547999d3865cf3953086fae84ef25
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2022-12-13 12:18:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ window.addEventListener("DOMContentLoaded", game);
 //General sprite load
 var sprite = new Image();
 var spriteExplosion = new Image();
-sprite.src = 'https://marclopezavila.github.io/planet-defense-game/img/sprite.png';
+sprite.src = 'sprite.png';
 
 window.onload = function() {
     spriteExplosion.src = 'https://marclopezavila.github.io/planet-defense-game/img/explosion.png';
